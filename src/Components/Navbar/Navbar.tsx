a6faf9b68a9af908286480418a8de610498b9049
thanhoaiedunlu <21130360@st.hcmuaf.edu.vn>
2024-05-17 17:59:28 +0700

    day1

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import './Navbar.css'
 import logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png';
 const Navbar =() => {
     return(
         <div className='navbar'>
             <div className='navbar-logo'>
                 <img src={logo} alt="null"/>
                 <p>Shop</p>
+            </div>
+            <ul className='nav-menu'>
+                <li>Men <hr/></li>
+                <li>Home</li>
+                <li>Women</li>
+                <li>Kid</li>
+            </ul>
+            <div className='nav-login-cart'>
+                <button>Login</button>
+                <img src={cart_icon} alt="null"/>
             </div>
         </div>
     )
