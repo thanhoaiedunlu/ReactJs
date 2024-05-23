78746260f8a54fd2c73472aaf2a40439df101328
thanhoaiedunlu <21130360@st.hcmuaf.edu.vn>
2024-05-23 19:03:33 +0700

    day5

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,16 +1,25 @@
-import React, { createContext } from 'react';
+import React, {createContext, useState} from 'react';
 import all_product from '../Components/Assets/all_product'; // Đảm bảo đường dẫn chính xác
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = (allProducts) => {
+    let cart = {};
+    for (let index = 0; index < allProducts.length; index++) {
+        cart[index] = 0;
+    }
+    return cart;
+}
+
 const ShopContextProvider = (props) => {
-    const contextValue = { all_product };
+    const [cartItems, setCartItems] = useState(getDefaultCart(all_product));
+    const contextValue = {all_product, cartItems };
+    console.log(cartItems)
     return (
-          <ShopContext.Provider value={contextValue}>
-              {props.children}
-          </ShopContext.Provider>
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
     );
 };
 
 export default ShopContextProvider;
-
